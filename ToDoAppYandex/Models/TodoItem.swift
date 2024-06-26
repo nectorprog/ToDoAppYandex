4735effc594c72a7c2b1bdfa2b624ab3d981f084
nectorprog <nector.dev@gmail.com>
2024-06-26 11:26:32 +0300

    Refactor TodoItem structure

--- ToDoAppYandex/Models/TodoItem.swift
+++ ToDoAppYandex/Models/TodoItem.swift
@@ -9,13 +9,15 @@ struct TodoItem {
     let createdAt: Date
     let updatedAt: Date?
     
-    init(id: String = UUID().uuidString,
+    init(
+        id: String = UUID().uuidString,
         text: String,
         importance: Importance,
         deadline: Date? = nil,
-        isReady: Bool,
-        createdAt: Date,
-        updatedAt: Date? = nil) {
+        isReady: Bool = false,
+        createdAt: Date = Date(),
+        updatedAt: Date? = nil
+    ) {
         self.id = id
         self.text = text
         self.importance = importance
@@ -23,5 +25,63 @@ struct TodoItem {
         self.isReady = isReady
         self.createdAt = createdAt
         self.updatedAt = updatedAt
+    }
+    
+    var dict: [String: Any] {
+        var jsonDict = [String: Any]()
+        jsonDict["id"] = id
+        jsonDict["text"] = text
+        
+        if importance != .medium {
+            jsonDict["importance"] = importance.rawValue
+        }
+        
+        if let deadline {
+            jsonDict["deadline"] = deadline.timeIntervalSince1970
+        }
+        
+        jsonDict["isReady"] = isReady
+        jsonDict["createdAt"] = createdAt.timeIntervalSince1970
+        
+        if let updatedAt {
+            jsonDict["updatedAt"] = updatedAt.timeIntervalSince1970
+        }
+        
+        return jsonDict
+    }
+    
+    static func from(dict: [String: Any]) -> TodoItem? {
+        guard let id = dict["id"] as? String,
+              let text = dict["text"] as? String,
+              let isReady = dict["isReady"] as? Bool,
+              let createdAtTimeInterval = dict["createdAt"] as? TimeInterval else {
+            return nil
+        }
+        
+        let createdAt = Date(timeIntervalSince1970: createdAtTimeInterval)
+        
+        let importance: Importance
+        if let importanceString = dict["importance"] as? String,
+           let imp = Importance(rawValue: importanceString) {
+            importance = imp
+        } else {
+            importance = .medium
+        }
+        
+        let deadline: Date?
+        if let deadlineTimeInterval = dict["deadline"] as? TimeInterval {
+            deadline = Date(timeIntervalSince1970: deadlineTimeInterval)
+        } else {
+            deadline = nil
+        }
+        
+        let updatedAt: Date?
+        if let updatedAtTimeInterval = dict["updatedAt"] as? TimeInterval {
+            updatedAt = Date(timeIntervalSince1970: updatedAtTimeInterval)
+        } else {
+            updatedAt = nil
+        }
+
+        return TodoItem(id: id, text: text, importance: importance, deadline: deadline, isReady: isReady, createdAt: createdAt, updatedAt: updatedAt)
     }
 }
