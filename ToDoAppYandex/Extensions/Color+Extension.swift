1fd38cf774d70b1e222107ba77fee0e5b44436d0
nectorprog <nector.dev@gmail.com>
2024-07-03 17:44:28 +0300

    add custom colors

---
+++ ToDoAppYandex/Extensions/Color+Extension.swift
@@ -0,0 +1,24 @@
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let bElevated = Color("backElevated")
+    static let biPrimary = Color("backIosPrimary")
+    static let bPrimary = Color("backPrimary")
+    static let bSecondary = Color("backSecondary")
+    
+    static let cBlue = Color("colorBlue")
+    static let cGray = Color("colorGray")
+    static let cGrayLight = Color("colorGrayLight")
+    static let cGreen = Color("colorGreen")
+    static let cRed = Color("colorRed")
+    static let cWhite = Color("colorWhite")
+    
+    static let lDisable = Color("labelDisable")
+    static let lPrimary = Color("labelPrimary")
+    static let lSecondary = Color("labelSecondary")
+    static let lTertiary = Color("labelTertiary")
+    static let sNavBarBlur = Color("supportNavBarBlur")
+    static let sOverlay = Color("supportOverlay")
+    static let sSeparatior = Color("supportSeparator")
+}
